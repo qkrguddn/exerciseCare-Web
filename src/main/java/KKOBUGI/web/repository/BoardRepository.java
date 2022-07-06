6a37dc4bae71ac4be567a49e98a817d584568500
HaJungSu <matt1235@naver.com>
2022-07-06 10:46:05 +0900

    Board Class 수정, 삭제 로직 추가

--- src/main/java/KKOBUGI/web/repository/BoardRepository.java
+++ src/main/java/KKOBUGI/web/repository/BoardRepository.java
@@ -13,11 +13,24 @@ public class BoardRepository {
 
     private final EntityManager em;
 
+    /** board 저장
+     * */
     public void save(Board board){
         em.persist(board);
     }
 
-    // user_Id 로 find
+
+    /** 게시판 삭제
+     * */
+    public void remove(Long boardId) {
+        em.createQuery("delete from Board b where b.id = :id").setParameter("id", boardId);
+    }
+
+
+    /**
+     * 게시판 조회
+     * */
+    // userId 로 find
     public List<Board> findAll(Long userId){
         return em.createQuery("select b from Board b join b.user u where u.id = :userId")
                 .setParameter("userId", userId)
@@ -33,6 +46,8 @@ public class BoardRepository {
     // title 로 찾음
     public List<Board> findByTitle(String title){
         return em.createQuery("select b from Board b where b.title = :title")
-                .setParameter("title", title).getResultList();
+                .setParameter("title", title).setMaxResults(20)
+                .getResultList();
     }
+
 }
--- src/main/java/KKOBUGI/web/service/BoardService.java
+++ src/main/java/KKOBUGI/web/service/BoardService.java
@@ -1,12 +1,64 @@
 package KKOBUGI.web.service;
 
+import KKOBUGI.web.domain.Board;
+import KKOBUGI.web.domain.User;
+import KKOBUGI.web.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    /**
+     * board 저장  */
+    @Transactional
+    public Long save(Board board){
+        boardRepository.save(board);
+        return board.getId();
+    }
+
+    /**
+     *  게시판 수정  */
+    @Transactional
+    public void fixBoard(Long boardId, String title, String content, LocalDateTime modifyDate){
+        Board board = boardRepository.findOne(boardId);
+        board.setTitle(title);
+        board.setContent(content);
+        board.setModifyDate(modifyDate);
+    }
+
+    /**
+     * 게시판 삭제  */
+    @Transactional
+    public void remove(Long boardId){
+        boardRepository.remove(boardId);
+    }
+
+
+    /**
+     * 게시판 조회
+     * */
+    // 한 유저가 작성한 게시판 모두 불러옴
+    public List<Board> findAll(User user){
+        return boardRepository.findAll(user.getId());
+    }
+
+    // board id로 찾음
+    public Board findOne(Long id){
+        return boardRepository.findOne(id);
+    }
+
+    // 제목으로 찾음
+    public List<Board> findByTitle(String title){
+        return boardRepository.findByTitle(title);
+    }
 
 }
