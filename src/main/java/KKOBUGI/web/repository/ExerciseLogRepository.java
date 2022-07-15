92ce31b623fb40cecf6d4335cfcafac9936030cf
HaJungSu <matt1235@naver.com>
2022-07-15 21:09:10 +0900

    create ExerciseLog Service, Repository

--- src/main/java/KKOBUGI/web/repository/ExerciseLogRepository.java
+++ src/main/java/KKOBUGI/web/repository/ExerciseLogRepository.java
@@ -1,4 +1,46 @@
 package KKOBUGI.web.repository;
 
+import KKOBUGI.web.domain.dto.ExerciseLogDto;
+import KKOBUGI.web.domain.entity.Exercise;
+import KKOBUGI.web.domain.entity.ExerciseLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class ExerciseLogRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 저장 */
+    public Long save(ExerciseLog exerciseLog) {
+        em.persist(exerciseLog);
+        return exerciseLog.getId();
+    }
+
+    /**
+     * 조회 */
+    public ExerciseLog find(Long id){
+        return em.find(ExerciseLog.class, id);
+    }
+
+    /**
+     * 수정 */
+    public Long fix(Long id, ExerciseLogDto.ReqDtoList req){
+        ExerciseLog exLog = em.find(ExerciseLog.class, id);
+        exLog.fixExerciseLog(req);
+        return exLog.getId();
+    }
+
+    /**
+     * 삭제 */
+    public Long delete(Long id){
+        ExerciseLog exerciseLog = em.find(ExerciseLog.class, id);
+        em.remove(exerciseLog);
+        return exerciseLog.getId();
+    }
 }
--- src/main/java/KKOBUGI/web/service/ExerciseLogService.java
+++ src/main/java/KKOBUGI/web/service/ExerciseLogService.java
@@ -1,4 +1,47 @@
 package KKOBUGI.web.service;
 
+import KKOBUGI.web.domain.dto.ExerciseLogDto;
+import KKOBUGI.web.domain.entity.Exercise;
+import KKOBUGI.web.domain.entity.ExerciseLog;
+import KKOBUGI.web.repository.ExerciseLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ExerciseLogService {
+
+    private ExerciseLogRepository exerciseLogRepository;
+
+    /**
+     * 저장 */
+    @Transactional
+    public Long save(ExerciseLog exerciseLog){
+        return exerciseLogRepository.save(exerciseLog);
+    }
+
+
+    /**
+     * 조회 */
+    public ExerciseLog find(Long id){
+        return exerciseLogRepository.find(id);
+    }
+
+    /**
+     * 수정 */
+    @Transactional
+    public Long fix(Long id, ExerciseLogDto.ReqDtoList req){
+        return exerciseLogRepository.fix(id, req);
+    }
+
+    /**
+     * 삭제
+     */
+    @Transactional
+    public Long delete(Long id){
+        return exerciseLogRepository.delete(id);
+    }
+
 }
